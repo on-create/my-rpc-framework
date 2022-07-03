02c103d8f0e440153920bccc58fb7375af904c5f
on-create <zhong.ming.shan@outlook.com>
2022-07-03 20:22:00 +0800

    修改RpcRequestHandler

--- rpc-framework-simple/src/main/java/org/example/simple/handler/RpcRequestHandler.java
+++ rpc-framework-simple/src/main/java/org/example/simple/handler/RpcRequestHandler.java
@@ -3,6 +3,7 @@ package org.example.simple.handler;
 import org.example.common.dto.RpcRequest;
 import org.example.common.dto.RpcResponse;
 import org.example.common.enumeration.RpcResponseCode;
+import org.example.common.exception.RpcException;
 import org.example.simple.provider.ServiceProvider;
 import org.example.simple.provider.impl.ServiceProviderImpl;
 import org.slf4j.Logger;
@@ -26,34 +27,37 @@ public class RpcRequestHandler {
      * @return
      */
     public Object handle(RpcRequest rpcRequest) {
-        Object result = null;
         // 通过注册中心获取到目标类
         Object service = SERVICE_PROVIDER.getServiceProvider(rpcRequest.getInterfaceName());
-        try {
-            result = invokeTargetMethod(rpcRequest, service);
-            logger.info("service:{} successful invoke method:{}", rpcRequest.getInterfaceName(), rpcRequest.getMethodName());
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            logger.error("occur org.example.simple.handler.RpcRequestHandler.exception", e);
-        }
+        Object result = invokeTargetMethod(rpcRequest, service);
+        logger.info("service:{} successful invoke method:{}", rpcRequest.getInterfaceName(), rpcRequest.getMethodName());
         return result;
     }
 
     /**
      * 根据 rpcRequest 和 service 对象特定的方法并返回结果
-     * @param rpcRequest
-     * @param service
+     * @param rpcRequest 客户端请求
+     * @param service 提供服务的对象
      * @return
-     * @throws NoSuchMethodException
-     * @throws InvocationTargetException
-     * @throws IllegalAccessException
      */
-    private Object invokeTargetMethod(RpcRequest rpcRequest, Object service) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method;
+    private Object invokeTargetMethod(RpcRequest rpcRequest, Object service) {
+        /*Method method;
         try {
             method = service.getClass().getMethod(rpcRequest.getMethodName(), rpcRequest.getParamTypes());
         } catch (NoSuchMethodException e) {
             return RpcResponse.fail(RpcResponseCode.NOT_FOUND_METHOD);
         }
-        return method.invoke(service, rpcRequest.getParameters());
+        return method.invoke(service, rpcRequest.getParameters());*/
+
+        Object result;
+        try {
+            Method method = service.getClass().getMethod(rpcRequest.getMethodName(), rpcRequest.getParamTypes());
+            result = method.invoke(service, rpcRequest.getParameters());
+        } catch (NoSuchMethodException e) {
+            return RpcResponse.fail(RpcResponseCode.NOT_FOUND_METHOD);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RpcException(e.getMessage(), e);
+        }
+        return result;
     }
 }
