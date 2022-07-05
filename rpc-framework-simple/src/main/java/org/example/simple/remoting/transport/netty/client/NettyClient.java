6ea9a8d901cb68a9a89d36b79080c93087982042
on-create <zhong.ming.shan@outlook.com>
2022-07-05 10:08:17 +0800

    更新ChannelProvider

--- rpc-framework-simple/src/main/java/org/example/simple/remoting/transport/netty/client/NettyClient.java
+++ rpc-framework-simple/src/main/java/org/example/simple/remoting/transport/netty/client/NettyClient.java
@@ -1,12 +1,11 @@
 package org.example.simple.remoting.transport.netty.client;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.example.simple.remoting.dto.RpcRequest;
 import org.example.simple.remoting.dto.RpcResponse;
@@ -14,18 +13,21 @@ import org.example.simple.serialize.kryo.KryoSerializer;
 import org.example.simple.remoting.transport.netty.codec.kyro.NettyKryoDecoder;
 import org.example.simple.remoting.transport.netty.codec.kyro.NettyKryoEncoder;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+
 @Slf4j
 public final class NettyClient {
 
-    private static final Bootstrap b;
+    private static final Bootstrap bootstrap;
     private static final EventLoopGroup eventLoopGroup;
 
     // 初始化相关资源
     static {
         eventLoopGroup = new NioEventLoopGroup();
-        b = new Bootstrap();
+        bootstrap = new Bootstrap();
         KryoSerializer kryoSerializer = new KryoSerializer();
-        b.group(eventLoopGroup)
+        bootstrap.group(eventLoopGroup)
                 .channel(NioSocketChannel.class)
                 // 连接的超时时间，超过这个时间还是建立不上的话则代表连接失败
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
@@ -45,12 +47,23 @@ public final class NettyClient {
                 });
     }
 
+    @SneakyThrows
+    public Channel doConnect(InetSocketAddress inetSocketAddress) {
+        CompletableFuture<Channel> completableFuture = new CompletableFuture<>();
+        bootstrap.connect(inetSocketAddress)
+                .addListener((ChannelFutureListener) future -> {
+                    if (future.isSuccess()) {
+                        log.info("客户端连接成功!");
+                        completableFuture.complete(future.channel());
+                    } else {
+                        throw new IllegalStateException();
+                    }
+                });
+        return completableFuture.get();
+    }
+
     public static void close() {
         log.info("call close method");
         eventLoopGroup.shutdownGracefully();
-    }
-
-    public static Bootstrap getBootstrap() {
-        return b;
     }
 }
